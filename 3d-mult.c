2866f5cee4937e347454d4941014680ee245dae9
Nisan Chiprut <nisan.chiprut@mail.huji.ac.il>
2018-01-18 16:54:08 +0200

    fixed bug in mult-3d
    Signed-off-by: Nisan Chiprut <nisan.chiprut@mail.huji.ac.il>

--- 3d-mult.c
+++ 3d-mult.c
@@ -6,14 +6,64 @@
 #define MAT_SIZE 5
 #define EPS 0.00000001
 
+void mult(double *a, double *b, double *c) {
+
+	for (int i = 0; i < MAT_SIZE; ++i) {
+		for (int j = 0; j < MAT_SIZE; ++j) {
+			c[MAT_SIZE*i + j] = 0;
+
+			for (int k = 0; k < MAT_SIZE; ++k) {
+				c[MAT_SIZE*i + j] += a[MAT_SIZE*i + k] * b[MAT_SIZE*k + j];
+			}
+		}
+	}
+}
+
+void printMat(double *matA, double *matB, double *matC){
+
+	//if ((coords3D[0] == 2)&&(coords3D[1] == 0)&&(coords3D[2] == 0)){
+	for (int i = 0; i<MAT_SIZE; ++i) {
+		for (int j = 0; j<MAT_SIZE; ++j) {
+			printf("%f ", matA[i*MAT_SIZE+j]);
+		}
+		printf("\n ");
+	}
+	printf("\n ");
+	for (int i = 0; i<MAT_SIZE; ++i) {
+		for (int j = 0; j<MAT_SIZE; ++j) {
+			printf("%f ", matB[i*MAT_SIZE+j]);
+		}
+		printf("\n ");
+	}
+	printf("\n ");
+	for (int i = 0; i<MAT_SIZE; ++i) {
+		for (int j = 0; j<MAT_SIZE; ++j) {
+			printf("%f ", matC[i*MAT_SIZE+j]);
+		}
+		printf("\n ");
+	}
+}
+
+void printMatA(double *matA){
+
+	//if ((coords3D[0] == 2)&&(coords3D[1] == 0)&&(coords3D[2] == 0)){
+	for (int i = 0; i<MAT_SIZE; ++i) {
+		for (int j = 0; j<MAT_SIZE; ++j) {
+			printf("%f ", matA[i*MAT_SIZE+j]);
+		}
+		printf("\n ");
+	}
+}
+
 int main(int argc, char **argv) {
 	int rank, size;
 	int ndim = 3;
 	int period[3] = {0};
 	int dims[3];
-	double buf[MAT_SIZE * MAT_SIZE] = {0};
+	double matC[MAT_SIZE * MAT_SIZE] = {0};
 	double matA[MAT_SIZE * MAT_SIZE] = {0};
 	double matB[MAT_SIZE * MAT_SIZE] = {0};
+
 	
 	MPI_Comm comm3D, commRow, commCol, commHeight;
 	
@@ -47,6 +97,7 @@ int main(int argc, char **argv) {
 	MPI_Comm_rank(comm3D, &id3D);
 	MPI_Cart_coords(comm3D, id3D, ndim, coords3D);
 
+
 	/* Create 1D row subgrids */
 	belongs[0] = 1; belongs[1] = 0; belongs[2] = 0;
 	MPI_Cart_sub(comm3D, belongs, &commRow);
@@ -71,7 +122,23 @@ int main(int argc, char **argv) {
 	origin[1] = coords3D[1];
 	origin[2] = coords3D[2];
 
-	srand(rank+1);
+	srand(rank+2);
+	if (coords3D[1] == 0) {
+		// generate matrix
+		for (int i = 0; i < MAT_SIZE * MAT_SIZE; ++i) {
+			matB[i] = (rand() % 100) -50;
+		}
+
+		MPI_Cart_rank(commCol, coords3D + 1, &originRank);
+		MPI_Ibcast(matB, MAT_SIZE*MAT_SIZE, MPI_DOUBLE, originRank, commCol, &requests[1]);
+		
+	} else {
+		origin[1] = 0;
+		MPI_Cart_rank(commCol, origin + 1, &originRank);
+		MPI_Ibcast(matB, MAT_SIZE*MAT_SIZE, MPI_DOUBLE, originRank, commCol, &requests[1]);
+		origin[1] = coords3D[1];
+
+	}
 	if (coords3D[0] == 0) {
 		// generate matrix
 		for (int i = 0; i < MAT_SIZE * MAT_SIZE; ++i) {
@@ -87,24 +154,11 @@ int main(int argc, char **argv) {
 		origin[0] = coords3D[0];
 
 	}
-	if (coords3D[1] == 0) {
-		// generate matrix
-		for (int i = 0; i < MAT_SIZE * MAT_SIZE; ++i) {
-			matB[i] = (rand() % 100) -50;
-		}
 
-		MPI_Cart_rank(commCol, coords3D, &originRank);
-		MPI_Ibcast(matB, MAT_SIZE*MAT_SIZE, MPI_DOUBLE, originRank, commCol, &requests[1]);
-	} else {
-		origin[1] = 0;
-		MPI_Cart_rank(commCol, origin, &originRank);
-		MPI_Ibcast(matB, MAT_SIZE*MAT_SIZE, MPI_DOUBLE, originRank, commCol, &requests[1]);
-		origin[1] = coords3D[1];
-
-	}
-
-	MPI_Wait(&requests[0], &status[0]);
 	MPI_Wait(&requests[1], &status[1]);
+	MPI_Wait(&requests[0], &status[0]);
+
+	mult(matA, matB, matC);
 
 	/*
 	int origin[3] = {0};
@@ -132,17 +186,12 @@ int main(int argc, char **argv) {
 	}
 	*/
 
-	if (((coords3D[0] == 2)&&(coords3D[1] == 0)&&(coords3D[2] == 0)) || ((coords3D[0] == 1)&&(coords3D[1] == 0)&&(coords3D[2] == 0))){
-		for (int i = 0; i<MAT_SIZE; ++i) {
-			for (int j = 0; j<MAT_SIZE; ++j) {
-				printf("%f ", matA[i*MAT_SIZE+j]);
-			}
-			printf("\n ");
-		}
+	if ((coords3D[0] == 1) && (coords3D[1] == 0) && (coords3D[2] == 0)) {
+		printMat(matA, matB, matC);
 	}
+
 
 	MPI_Finalize();
 	return 0;
 }
 	
-
