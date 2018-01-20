b2330afdf2412b1c7c7def934d6be766775354ed
Nisan Chiprut <nisan.chiprut@mail.huji.ac.il>
2018-01-20 16:53:36 +0200

    finished grid.

--- grid.c
+++ grid.c
@@ -2,8 +2,33 @@
 #include <math.h>
 #include <stdlib.h>
 #include "mpi.h"
+#include <sys/stat.h>
 
 #define VEC_SIZE 5
+#define DIRERCTORY "grid"
+#define EPS 0.00000001
+
+void saveVec(char* dir, char* type, int row, int col, double* out) { 
+
+	// open input file and save the matrix
+	char szFileName[255] = {0};
+	sprintf(szFileName, "%s/mult-3d_%s_%d_%d.txt", dir, type, row, col);
+	FILE *f = fopen(szFileName, "w");
+	for(int i = 0; i< VEC_SIZE; ++i){
+		fprintf(f, "%lf ", out[i]);
+	}
+	fclose(f);
+	
+}
+
+void conv(double* input, double* output) {
+	for (int i=0; i<VEC_SIZE; ++i) {
+		for (int j=1; j<=4; ++j) {
+			output[i] += j*input[i+(j-1)*VEC_SIZE];
+
+		}
+	}
+}
 
 int getRank(int x, int y, int root) {
 	x += root;
@@ -22,13 +47,14 @@ int main(int argc, char **argv) {
 
 	// make sure the number of processes has root
 	float s = sqrt(size);
-	if ((ceil(s) -s) != 0) {
+	if ((ceil(s) -s) >= EPS) {
 		if (rank == 0) {
 			printf("%d has no root\n", size);
 		}
 		MPI_Finalize();
 		return 0;
 	}
+	mkdir(DIRERCTORY, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
  
 	// generate vector
 	srand(rank+1);
@@ -42,24 +68,17 @@ int main(int argc, char **argv) {
 	int y = ((int)rank % (int)s);
 
 	// open input file and save the matrix
-	char szFileName[255] = {0};
-	sprintf(szFileName, "grid_input_%d_%d.txt", x,y);
-	FILE *f = fopen(szFileName, "w");
-	for(int i = 1; i<= VEC_SIZE; ++i){
-		fprintf(f, "%lf ", vec[i-1]);
-	}
+	saveVec(DIRERCTORY, "input", x, y, vec);
 	
 	double buf[VEC_SIZE*4];
 
-	int neighb[4] = {getRank(x,y+1,s),getRank(x+1,y,s),getRank(x,y-1,s),getRank(x-1,y,s)};
+	int neighb[4] = {getRank(x-1,y,s),getRank(x+1,y,s),getRank(x,y-1,s),getRank(x,y+1,s)};
 	MPI_Request requests[4];
 	MPI_Request send_requests[4];
 	MPI_Status status[4];
 
 	for (int i = 0; i < 4; ++i) {
 
-		printf("my rank: %d, sent to: %d\n", rank, neighb[i]);
-		fflush(stdout);
 		MPI_Isend(vec, VEC_SIZE, MPI_DOUBLE, neighb[i], 0, MPI_COMM_WORLD, &send_requests[i]);
 		MPI_Irecv(buf+(i*VEC_SIZE), VEC_SIZE, MPI_DOUBLE, neighb[i], 0, MPI_COMM_WORLD, &requests[i]);
 	}
@@ -67,25 +86,9 @@ int main(int argc, char **argv) {
 
 		MPI_Wait(&requests[i], &status[i]);
 	}
-	if (rank == 0) {
-		for(int i = 1; i<= VEC_SIZE*4; ++i){
-			printf("%lf ", buf[i-1]);
-			if ((i%5) ==0){
-				printf("\n");
-			}
-
-		}
-		
-	}
+	conv(buf, vec);
+	saveVec(DIRERCTORY, "output", x, y, vec);
 	
-	/*
-	// open output file and save the matrix
-	sprintf(szFileName, "grid_output_%d_%d.txt", y,x);
-	f = fopen(szFileName, "w");
-	for(int i = 1; i<= VEC_SIZE; ++i){
-		fprintf(f, "%lf ", vec[i-1]);
-	}
-	*/
 
 	MPI_Finalize();
 	return 0;
